ace20aee02cca2a6708d2dac98683961fff8e828
Salaheddine Naslouby <naslouby.salahe@gmail.com>
2026-09-04 23:07:53 +0100

    Delete FullMethodSupport adjudication construct from registry

--- src/fedcampaign_emhi/experiments/registry.py
+++ src/fedcampaign_emhi/experiments/registry.py
@@ -1,4 +1,3 @@
-import statistics
 from dataclasses import dataclass
 
 from fedcampaign_emhi.config.schema import LoadedScientificConfiguration, ScientificConfig
@@ -6,9 +5,6 @@ from fedcampaign_emhi.domain.enums import ExecutionRole, ExperimentName, MethodN
 from fedcampaign_emhi.domain.types import (
     ArtifactFilename,
     Boolean,
-    OdiRateAdvantage,
-    OperationalLeadEpochs,
-    Probability,
     ResumeStep,
     SeedCount,
     SeedValue,
@@ -31,93 +27,6 @@ class ExperimentContract:
     methods: tuple[MethodName, ...]
     uses_real_seeds: Boolean
     uses_synthetic_seeds: Boolean
-
-
-@dataclass(frozen=True)
-class FullMethodSupportInputs:
-    heldout_pfa_upper_bound: Probability
-    target_pfa: Probability
-    mean_strict_odi_rate: Probability
-    minimum_strict_odi_rate: Probability
-    paired_odi_advantage: OdiRateAdvantage
-    minimum_odi_advantage: OdiRateAdvantage
-    median_lead_among_successes: OperationalLeadEpochs
-    minimum_median_lead: OperationalLeadEpochs
-    directional_adjusted_p_value: Probability
-    nominal_alpha: Probability
-    full_operating_point_available: Boolean
-    comparator_operating_point_available: Boolean
-
-
-@dataclass(frozen=True)
-class FullMethodSupportResult:
-    pfa_criterion_satisfied: Boolean
-    odi_rate_criterion_satisfied: Boolean
-    advantage_criterion_satisfied: Boolean
-    lead_criterion_satisfied: Boolean
-    directional_criterion_satisfied: Boolean
-    matched_operating_point_criterion_satisfied: Boolean
-
-    @property
-    def all_criteria_pass(self) -> Boolean:
-        return (
-            self.pfa_criterion_satisfied
-            and self.odi_rate_criterion_satisfied
-            and self.advantage_criterion_satisfied
-            and self.lead_criterion_satisfied
-            and self.directional_criterion_satisfied
-            and self.matched_operating_point_criterion_satisfied
-        )
-
-
-def strict_odi_rate_criterion(mean_odi_rate: Probability, minimum_rate: Probability) -> Boolean:
-    return mean_odi_rate >= minimum_rate
-
-
-def paired_odi_advantage_criterion(
-    paired_advantage: OdiRateAdvantage,
-    minimum_advantage: OdiRateAdvantage,
-) -> Boolean:
-    return paired_advantage >= minimum_advantage
-
-
-def median_operational_lead_criterion(
-    median_lead_epochs: OperationalLeadEpochs,
-    minimum_lead_epochs: OperationalLeadEpochs,
-) -> Boolean:
-    return median_lead_epochs >= minimum_lead_epochs
-
-
-def median_of(values: tuple[OperationalLeadEpochs, ...]) -> OperationalLeadEpochs:
-    if not values:
-        raise ValueError("median requires at least one value")
-    return statistics.median(values)
-
-
-def matched_operating_point_requirement(
-    full_method_available: Boolean,
-    comparator_available: Boolean,
-) -> Boolean:
-    return full_method_available and comparator_available
-
-
-def evaluate_full_method_support(inputs: FullMethodSupportInputs) -> FullMethodSupportResult:
-    return FullMethodSupportResult(
-        pfa_criterion_satisfied=inputs.heldout_pfa_upper_bound <= inputs.target_pfa,
-        odi_rate_criterion_satisfied=strict_odi_rate_criterion(
-            inputs.mean_strict_odi_rate, inputs.minimum_strict_odi_rate
-        ),
-        advantage_criterion_satisfied=paired_odi_advantage_criterion(
-            inputs.paired_odi_advantage, inputs.minimum_odi_advantage
-        ),
-        lead_criterion_satisfied=median_operational_lead_criterion(
-            inputs.median_lead_among_successes, inputs.minimum_median_lead
-        ),
-        directional_criterion_satisfied=inputs.directional_adjusted_p_value < inputs.nominal_alpha,
-        matched_operating_point_criterion_satisfied=matched_operating_point_requirement(
-            inputs.full_operating_point_available, inputs.comparator_operating_point_available
-        ),
-    )
 
 
 def experiment_registry(config: ScientificConfig) -> tuple[ExperimentContract, ...]:
